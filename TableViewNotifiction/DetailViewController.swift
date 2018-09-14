bb13b3ff1257392b36b26e0ccacf1a64d309154e
Son <>
2018-09-15 02:39:12 +0700

    no message

--- TableViewNotifiction/DetailViewController.swift
+++ TableViewNotifiction/DetailViewController.swift
@@ -4,7 +4,8 @@ import UIKit
 class DetailViewController: UIViewController {
     
     @IBOutlet weak var txtData: UITextField!
-    var bucket: String?
+    var bucket: String!
+    //    var space = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -13,10 +14,47 @@ class DetailViewController: UIViewController {
         }
     }
     
+    //    func checkSpace() -> Bool {
+    //        var s = ""
+    //        for _ in 1 ... bucket.count {
+    //            s += " "
+    //        }
+    //        space.append(s)
+    //        return true
+    //    }
+    //
     @IBAction func btnDone(_ sender: Any) {
-        bucket = txtData.text
-        NotificationCenter.default.post(name: .done, object: bucket)
-        navigationController?.popViewController(animated: true)
+        
+        bucket = txtData.text!
+        let trimmed = (txtData.text)!.trimmingCharacters(in: .whitespaces)
+        if (bucket.isEmpty) {
+            let alert = UIAlertController(title: "Thong Bao", message: "Xin moi ban nhap ten ", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default))
+            self.present(alert, animated: true, completion: nil)
+        }
+        else{
+            var space = ""
+            var s = ""
+            for _ in 1 ... bucket.count {
+                s += " "
+            }
+            space.append(s)
+            if bucket == space{
+                print("eroor")
+                let alert = UIAlertController(title: "Thong Bao", message: "Xin moi ban nhap ten ", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default))
+                self.present(alert, animated: true, completion: nil)
+            }
+            else {
+                
+                NotificationCenter.default.post(name: .done, object: trimmed)
+                navigationController?.popViewController(animated: true)
+            }
+            
+        }
+        
     }
     
 }
